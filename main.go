15ed4e28e6f6996f60acce1ae66b11abcddc98be
Andrew Kraynak <abk89@miami.edu>
2020-12-12 12:12:29 -0500

    Updated program to execute commands

--- main.go
+++ main.go
@@ -2,13 +2,33 @@ package main
 
 import (
 	"fmt"
+	"strings"
+	"os"
+	"os/exec"
 )
 
 func main() {
 	for {
 		var line string
+		fmt.Print(">> ")
 		fmt.Scanln(&line)
-		fmt.Println("You entered " + line)
+		exec_cmd(line)
 	}
 }
 
+func exec_cmd(in string) error {
+	// remove newline and separate string
+	in = strings.TrimSuffix(in, "\n")
+	args := strings.Split(in, " ")
+	
+	switch args[0] {
+		case "exit":
+			os.Exit(0)
+	}
+
+	cmd := exec.Command(args[0], args[1:]...)	
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+
+	return cmd.Run()
+}
